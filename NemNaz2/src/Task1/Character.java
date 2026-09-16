4b067a16c935b360b666385341c3867b5e989488
ИльяGGFL <bambam.nem@mail.ru>
2026-09-16 09:20:40 +0300

    Add files via upload

---
+++ NemNaz2/src/Task1/Character.java
@@ -0,0 +1,23 @@
+package Task1;
+
+class Character {
+    int gold;
+    int silver;
+    double wood;
+    byte health;
+    byte helpersNumber;
+
+    public Character(
+            int characterGold,
+            int characterSilver,
+            double characterWood,
+            byte characterHealth,
+            byte characterHelpersNumber
+    ) {
+        gold = characterGold;
+        silver = characterSilver;
+        wood = characterWood;
+        health = characterHealth;
+        helpersNumber = characterHelpersNumber;
+    }
+}
